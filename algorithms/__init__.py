23aecfd8231e3d1514015f362d64f1d9198f4bcc
gumocimo <guglielmo.cimolai@icloud.com>
2025-08-18 23:08:45 +0200

    Update __init__.py

--- algorithms/__init__.py
+++ algorithms/__init__.py
@@ -1,13 +1,15 @@
 """Genetic algorithms for TSP.
 
-This module contains various algorithm implementations for solving TSP.
+This module contains various algorithm implementations for solving TSP,
+including Standard GA, Hybrid GA-ACO, and PSO.
 
 Author: gumocimo
-Date: 13/08/2025
+Date: 18/08/2025
 """
 
 from algorithms.base import TSPAlgorithm
 from algorithms.sga import StandardGA
 from algorithms.hga_aco import HybridGA_ACO
+from algorithms.pso import ParticleSwarmOptimization
 
-__all__ = ['TSPAlgorithm', 'StandardGA', 'HybridGA_ACO']
+__all__ = ['TSPAlgorithm', 'StandardGA', 'HybridGA_ACO', 'ParticleSwarmOptimization']
